c2a7385d9056aea9ad3329e1e27be60668bd8c42
Mateusz Urbaniak <matteus.urbaniak@hotmail.com>
2025-02-18 21:08:24 +0100

    Feat: Custom Observable

--- rxjs/src/app/app.component.ts
+++ rxjs/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import {Component, effect, OnDestroy, OnInit, signal} from '@angular/core';
-import {interval, map, Subscription, tap} from "rxjs";
+import {interval, map, Observable, Subscription, tap} from "rxjs";
 import {toObservable, toSignal} from "@angular/core/rxjs-interop";
 
 @Component({
@@ -17,6 +17,22 @@ export class AppComponent implements OnInit, OnDestroy {
   interval$ = interval(1000)
   intervalSignal = toSignal(this.interval$, {initialValue: 0})
 
+
+  // Custom Observable
+  customInterval$ = new Observable((subscriber) => {
+    let timesExecuted = 0
+    const interval = setInterval(() => {
+      if (timesExecuted > 3) {
+        clearInterval(interval)
+        subscriber.complete()
+        return
+      }
+      console.log('Emitting new value... ' + timesExecuted)
+      subscriber.next({message: 'New value'})
+      timesExecuted++
+    }, 2000)
+  })
+
   private _subscription?: Subscription;
 
 
@@ -32,6 +48,11 @@ export class AppComponent implements OnInit, OnDestroy {
           next: value => console.log(value),
           error: err => console.error(err)
         });*/
+
+    this._subscription = this.customInterval$.subscribe({
+      next: e => console.log(e),
+      complete: () =>  console.log('Completed')
+    })
   }
 
   ngOnDestroy() {
